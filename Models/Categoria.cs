31cf81b476201607b3b017503b57bdbf7c335b65
PabloSan1997 <jos_pab_997@hotmali.com>
2023-10-06 15:58:49 -0600

    Se agrego todo

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projectoef.Models;
 
 public class Categoria
 {
+    [Key]
     public Guid CategoriaId {get;set;}
+    
+    [Required]
+    [MaxLength(150)]
     public string Nombre {get;set;}
+
+    [MaxLength(1000)]
     public string Descripcion {get;set;}
+
+
     public virtual ICollection<Tarea> Tareas {get;set;}
 
 }

22213b460c8ec90594221128a666c9593f711bb8
PabloSan1997 <jos_pab_997@hotmali.com>
2023-10-06 13:52:09 -0600

    Se desarrollaron los modelos

---
+++ Models/Tarea.cs
@@ -0,0 +1,20 @@
+namespace projectoef.Models;
+
+public class Tarea
+{
+    public Guid TareaId { get; set; }
+    public Guid categoriaId { get; set; }
+    public string Titulo { get; set;}
+    public string Descripcion {get;set;}
+    public Prioridad PrioridadTarea {get;set;}
+    public DateTime FechaCreacion {get;set;}
+    public virtual Categoria Categoria {get;set;}
+}
+
+public enum Prioridad{
+    Baja,
+    Media,
+    Alta
+}
+ 
+ 
